95827aa8f40939b5cb72c63b7174b82a84e92e66
coleman7245 <dlcoleman1@live.com>
2026-07-27 22:47:11 -0600

    Added StyledBlock to the Block component.

--- frontend/src/pages/game/Block.tsx
+++ frontend/src/pages/game/Block.tsx
@@ -1,5 +1,15 @@
+import { prototypejs } from 'globals';
 import { styled } from 'styled-components';
 
-const StyledBlock = styled.div`
-    
+const StyledBlock = styled.div<{$image_url? : string}>`
+    width: 30px;
+    height: 30px;
+    border: 1px solid black;
+    background-color: grey;
 `;
+
+function Block({image_url} : {image_url : string}) {
+    return <StyledBlock><img src={image_url}></img></StyledBlock>
+}
+
+export default Block;
