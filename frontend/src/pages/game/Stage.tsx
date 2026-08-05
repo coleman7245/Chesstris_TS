72dbb2554a3205a3d2e8ef59fadc33c194057c06
coleman7245 <dlcoleman1@live.com>
2026-08-05 17:26:33 -0600

    Added the chess piece image pixel size to the game state.

--- frontend/src/pages/game/Stage.tsx
+++ frontend/src/pages/game/Stage.tsx
@@ -6,12 +6,13 @@ import { GameContext } from '../../App.tsx';
 import { TetrisPiece } from './TetrisPiece.tsx';
 import Block from './Block.tsx';
 import { BlockStatus } from '../../types.ts';
+import Vector2 from '../../classes/Vector2.ts';
 
-const StyledStage = styled.div<{blockSize : number, height : number, width : number}>`
+const StyledStage = styled.div<{blockSize : Vector2, height : number, width : number}>`
     display: grid;
     overflow: hidden;
-    grid-template-rows: repeat(${props => Math.floor(props.height / props.blockSize)}, 1fr);
-    grid-template-columns: repeat(${props => Math.floor(props.width / props.blockSize)}, 1fr);
+    grid-template-rows: repeat(${props => Math.floor(props.height / props.blockSize.y)}, 1fr);
+    grid-template-columns: repeat(${props => Math.floor(props.width / props.blockSize.x)}, 1fr);
     border: 10px solid white;
     outline: 10px solid black;
     position: relative;
@@ -50,7 +51,7 @@ function Stage({stage} : {stage : Array<Array<BlockStatus>>}) {
     const [gameState] = useContext(GameContext);
 
     return (
-        <StyledStage blockSize={30} height={gameState.stage_size.y} width={gameState.stage_size.x}>
+        <StyledStage blockSize={gameState.chess_piece_pixel_size} height={gameState.stage_size.y} width={gameState.stage_size.x}>
             {stage.map((row) => row.map((block, x) => <Block key={x} type={block['type']} image_url={block['image_url']} ></Block>))}
         </StyledStage>
     )
