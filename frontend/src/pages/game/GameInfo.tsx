e847b9bf45a3995072faf8ce0ddbfd8d88a95ba6
coleman7245 <dlcoleman1@live.com>
2026-08-19 17:13:51 -0600

    Deleted the GameInfo css file and moved it's contents to the styled component.

--- frontend/src/pages/game/GameInfo.tsx
+++ frontend/src/pages/game/GameInfo.tsx
@@ -1,18 +1,28 @@
 import { useContext } from 'react';
+import { styled } from 'styled-components';
 
 import { GameContext } from '../../App.tsx';
 
-import './GameInfo.css';
+const StyledGameInfo = styled.div`
+    position: absolute;
+    margin-top: 3%;
+    margin-left: 60%;
+    font-size: 50px;
+    border: 10px double black;
+    padding: 2%;
+    top: -10px;
+    left: -300px;
+`;
 
 function GameInfo() {
     const [gameState] = useContext(GameContext);
 
     return (
-        <div className='gameinfo'>
+        <StyledGameInfo className='gameinfo'>
             Player: {gameState.player_name} <br />
             Time: {gameState.finishTime.hours} : {gameState.finishTime.minutes} : {gameState.finishTime.seconds} <br />
             Score: {gameState.score}
-        </div>
+        </StyledGameInfo>
     )
 }
 
