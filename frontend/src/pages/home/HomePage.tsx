b5d82e08f57f098f70f5f5b021b9e1c2813b8d3b
coleman7245 <dlcoleman1@live.com>
2026-09-02 11:11:24 -0600

    Moved the styling for the home page from the css file to the styled component. Deleted the related css file.

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,35 +1,59 @@
 import { Link } from 'react-router-dom';
-
-import './HomePage.css';
-
+import { styled } from 'styled-components';
 import home_pic from '../../assets/images/chess_homepage_image.jpg';
+
+const StyledHomePage = styled.div`
+    font: 8em 'Georgia';
+    text-align: center;
+    color: black;
+    background-color: white;
+    border: 10px double black;
+    border-radius: 10px;
+    height: 800px;
+    width: 1400px;
+    margin: 0 5%;
+`;
+
+const StyledHomeImage = styled.img`
+    border: 10px double black;
+    size: 600px 600px;
+`;
+
+const StyledHomeButton = styled.button`
+    border: 10px double black;
+    background-color: white;
+    margin: 0 10px;
+    padding: 2% 2%;
+    border-radius: 10px;
+    font-weight: bold;
+`;
 
 function HomePage() {
     return (
-        <div className='homepage'>
+        <StyledHomePage>
             This is Chesstris <br />
-            <img src={home_pic} /> <br />
+            <StyledHomeImage src={home_pic} /> <br />
             <Link to='/game'>
-                <button>
+                <StyledHomeButton>
                     NEW GAME
-                </button>
+                </StyledHomeButton>
             </Link>
             <Link to='/rankings'>
-                <button>
-                    Rankings
-                </button>
+                <StyledHomeButton>
+                    RANKINGS
+                </StyledHomeButton>
             </Link>
             <Link to='/rules'>
-                <button>
-                    Rules
-                </button>
+                <StyledHomeButton>
+                    RULES
+                </StyledHomeButton>
             </Link>
             <Link to='/about'>
-                <button>
-                    About
-                </button>
+                <StyledHomeButton>
+                    ABOUT
+                </StyledHomeButton>
             </Link>
-        </div>
+        </StyledHomePage>
     );
 }
 
