e238aa4e42e6bff9395ea4efe8b12bd4fcea3835
coleman7245 <dlcoleman1@live.com>
2026-09-03 13:21:21 -0600

    Removed the about page css file and moved the styling to the related component.

--- frontend/src/pages/about/AboutPage.tsx
+++ frontend/src/pages/about/AboutPage.tsx
@@ -1,14 +1,27 @@
-import './AboutPage.css';
+import { styled } from 'styled-components';
 
 import Navbar from '../../shared_components/Navbar.tsx';
+
+const StyledAboutPage = styled.div`
+    font: 8em 'Georgia';
+    text-align: center;
+    color: black;
+    background-color: white;
+    border-bottom: 10px double black;
+    border-right: 10px double black;
+    border-left: 10px double black;
+    border-radius: 10px;
+    height: 800px;
+    width: 1487px;
+`;
 
 function AboutPage() {
     return (
         <>
             <Navbar />
-            <div className='aboutpage'>
+            <StyledAboutPage className='aboutpage'>
                 Text Here.
-            </div>
+            </StyledAboutPage>
         </>
     )
 }
